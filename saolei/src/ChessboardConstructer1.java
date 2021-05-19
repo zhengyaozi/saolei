88636619adc4bf1e3b05899e87e28957a896eadf
zhengyaozi <84397687+zhengyaozi@users.noreply.github.com>
2021-05-19 12:18:21 +0800

    Update ChessboardConstructer1.java

--- saolei/src/ChessboardConstructer1.java
+++ saolei/src/ChessboardConstructer1.java
@@ -17,7 +17,7 @@ import javax.swing.JMenuItem;  //这是 菜单项类
 //用于**单人游戏**时构建棋盘
 public class ChessboardConstructer1  implements ActionListener,Chessboard{
 
-    private static JFrame frame=new JFrame();;
+    private static JFrame frame=new JFrame();
 
     public static JFrame getJFrame() {
         return frame;
