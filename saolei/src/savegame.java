317ad9639fced0a52a674dd092447817021aa334
zhengyaozi <84397687+zhengyaozi@users.noreply.github.com>
2021-05-21 20:25:54 +0800

    ....

--- saolei/src/savegame.java
+++ saolei/src/savegame.java
@@ -3,21 +3,17 @@ import java.util.ArrayList;
 
 public class savegame {
     public savegame() throws IOException, ClassNotFoundException {
-
-
+        ObjectInputStream ois=new ObjectInputStream(new FileInputStream("C:\\Users\\zqlwcldzz\\IdeaProjects\\saolei\\oos.txt"));
+        ArrayList<ChessboardConstructer1> oldchessbroad=(ArrayList<ChessboardConstructer1>)ois.readObject();
+        ois.close();
+        ArrayList<ChessboardConstructer1> loadchessbroad=oldchessbroad;
 //      ArrayList<ChessboardConstructer1> loadchessbroad=returngame();
         ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream("C:\\Users\\zqlwcldzz\\IdeaProjects\\saolei\\oos.txt"));
-        ArrayList<ChessboardConstructer1> loadchessbroad=new ArrayList<>();
+
         loadchessbroad.add(begingame.game);
         oos.writeObject(loadchessbroad);
 
         oos.close();
     }
-// public ArrayList<ChessboardConstructer1> returngame() throws IOException, ClassNotFoundException {
-//     ObjectInputStream ois=new ObjectInputStream(new FileInputStream("C:\\Users\\zqlwcldzz\\IdeaProjects\\saolei\\oos.txt"));
-//     Object obj=ois.readObject();
-//     ArrayList<ChessboardConstructer1> loadchessbroad=(ArrayList<ChessboardConstructer1>)obj;
-//     ois.close();
-//     return  loadchessbroad;
-// }
+
 }
