6449dafa8edd4cd62fe10a44918917e57f33a86c
JJJJimmy-sustech <77683697+JJJJimmy-sustech@users.noreply.github.com>
2021-05-21 22:18:23 +0800

    双人模式
    双人模式更新

--- saolei/src/GameStat.java
+++ saolei/src/GameStat.java
@@ -4,8 +4,8 @@ class GameStat {
     public static ArrayList<String> user =new ArrayList<String>();
     public static ArrayList<String> passwd =new ArrayList<String>();
     public static int playerCnt=1;
-    public static int mapcolumn=1;
-    public static int maprow=1;
+    public static int mapcolumn=10;
+    public static int maprow=10;
     public static int maplei=1;
 
 }
---
+++ saolei/src/Player.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+public class Player {
+
+    public static String name;  //玩家姓名
+    public static ImageIcon picture=new ImageIcon();
+
+
+
+    public Player(String name,ImageIcon picture){
+        Player.name =name;
+        Player.picture=picture;
+    }
+
+
+    public JPanel PlayPane(int width,int height,int WhichPlayer){
+        JPanel player=new JPanel(null);
+        player.setSize(width,height);
+
+        picture=ShrinkPicture.AfterPicture(picture,100,100);
+
+        if(WhichPlayer==1){
+            JLabel jl=new JLabel(picture);
+            jl.setBounds(0,0,100,100);
+            jl.setVisible(true);
+            player.add(jl);
+
+
+
+        }
+
+        else {
+
+        }
+
+        player.setVisible(true);
+        return player;
+    }
+}
